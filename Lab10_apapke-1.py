a5157cfd949ba7f60c3f1c0c4eec07b32a157bce
Mya Applesauce <myaapplesauce42@gmail.com>
2026-04-05 17:11:08 -0400

    implemented relative paths

--- Lab10_apapke-1.py
+++ Lab10_apapke-1.py
@@ -14,22 +14,22 @@ def main():
         print(f"1. Dracula\n2. Antigone (R. C. Jebb translation)\n3. I Have No Mouth and I Must Scream\n4. The Prophecy\n5. Quit")
         choice = input(f"Select a text to analyze! (1-5) ")
         if choice == "1":
-            analysis = WordAnalyzer("/home/mya_applesauce/Documents/dracula.txt")
+            analysis = WordAnalyzer("dracula.txt")
             analysis.process_file()
             analysis.print_report()
             print("")
         elif choice == "2":
-            analysis = WordAnalyzer("/home/mya_applesauce/Documents/antigone.txt")
+            analysis = WordAnalyzer("antigone.txt")
             analysis.process_file()
             analysis.print_report()
             print("")
         elif choice == "3":
-            analysis = WordAnalyzer("/home/mya_applesauce/Documents/ihnmaims.txt")
+            analysis = WordAnalyzer("ihnmaims.txt")
             analysis.process_file()
             analysis.print_report()
             print("")
         elif choice == "4":
-            analysis = WordAnalyzer("/home/mya_applesauce/Documents/the_prophecy_of_deltarune.txt")
+            analysis = WordAnalyzer("the_prophecy_of_deltarune.txt")
             analysis.process_file()
             analysis.print_report()
             print("")
