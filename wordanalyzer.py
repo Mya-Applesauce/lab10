3454e4e20344cfc584d128b369e4281f64cc7f53
Mya Applesauce <myaapplesauce42@gmail.com>
2026-04-05 14:43:34 -0400

    refined and tested functions

--- wordanalyzer.py
+++ wordanalyzer.py
@@ -11,24 +11,28 @@ import string
 class WordAnalyzer:
     def __init__(self, filepath):
         self.__filepath = Path(filepath)
+        self.__frequencies = {}
 
     def process_file(self):
         try:
             Path.exists(self.__filepath)
-            doc = Path.open(self.__filepath)
-            table = str.maketrans(string.punctuation)
+            Path.open(self.__filepath)
+            doc = Path.read_text(self.__filepath)
+            table = str.maketrans("", "", string.punctuation)
             text = doc.translate(table)
-            text.lower()
-            text.split()
-            for word in text:
+            lower_text = text.lower()
+            split_text = lower_text.split()
+            print("split text:", split_text[:10])
+            #for word in split_text:
                 
         except FileNotFoundError:
             print("We don't got that text.")
 
-    #def print_report(self):
+    def print_report(self):
+        return self.process_file()
 
-#analysis = WordAnalyzer("the_prophecy_of_deltarune.txt")
-#print(analysis.process_file)
+analysis = WordAnalyzer("/home/mya_applesauce/Documents/the_prophecy_of_deltarune.txt")
+print(analysis.print_report())
 
 #if __name__ == "__main__":
     #analyst_list = []
