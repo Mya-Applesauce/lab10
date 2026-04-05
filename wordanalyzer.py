1fc77218c6195e91f2ff9a1d0c2b32732b636b99
Mya Applesauce <myaapplesauce42@gmail.com>
2026-04-04 22:32:54 -0400

    wrote the initaliazer

--- wordanalyzer.py
+++ wordanalyzer.py
@@ -5,3 +5,18 @@ word analyzer class
 04/04/26
 """
 
+from pathlib import Path
+
+class WordAnalyzer:
+    def __init__(self, filepath):
+        self.__filepath = Path(filepath)
+
+    def process_file(self):
+
+    def print_report(self):
+
+#if __name__ == "__main__":
+    #analyst_list = []
+    #for fname in ["moby_dick.txt", "typee.txt", "omoo.txt", "billy_budd.txt"]:
+        #analyst = WordAnalyzer(fname)
+        #analyst_list.append(analyst)
