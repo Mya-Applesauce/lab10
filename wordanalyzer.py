9ba7f08310d146b80861b7d23fe256b5c575d4d5
Mya Applesauce <myaapplesauce42@gmail.com>
2026-04-05 11:57:26 -0400

    started to set up the process_file method

--- wordanalyzer.py
+++ wordanalyzer.py
@@ -6,14 +6,29 @@ word analyzer class
 """
 
 from pathlib import Path
+import string
 
 class WordAnalyzer:
     def __init__(self, filepath):
         self.__filepath = Path(filepath)
 
     def process_file(self):
+        try:
+            Path.exists(self.__filepath)
+            doc = Path.open(self.__filepath)
+            table = str.maketrans(string.punctuation)
+            text = doc.translate(table)
+            text.lower()
+            text.split()
+            for word in text:
+                
+        except FileNotFoundError:
+            print("We don't got that text.")
 
-    def print_report(self):
+    #def print_report(self):
+
+#analysis = WordAnalyzer("the_prophecy_of_deltarune.txt")
+#print(analysis.process_file)
 
 #if __name__ == "__main__":
     #analyst_list = []
